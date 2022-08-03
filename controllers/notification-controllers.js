facae02a38b9f043f4d45d4845bc0af6d95bcd2c
reinhaaard13 <reinhardkevin.rk@gmail.com>
2022-08-03 15:19:38 +0700

    notification is now working

--- controllers/notification-controllers.js
+++ controllers/notification-controllers.js
@@ -1,72 +1,85 @@
 const db = require("../models");
 
 const makeNotification = async (entity, type) => {
-	const notification_object = await db.NotificationObject.create({
-		entity_type: type,
-		entity_id: entity.id || entity.comment_id,
-	});
-
-	const notification_change = await db.NotificationChange.create({
-		notification_object_id: notification_object.id,
-		actor_id: type === "TICKET_PROGRESS" ? entity.pic_id : entity.created_by || entity.user_id,
-	});
-
-	let notifier;
-
-	switch (type) {
-		case "TICKET_CREATE":
-			notifier = await db.User.findAll({
-				where: {
-					"$Role.role_category$": entity.assigned_to,
-				},
-				include: [{ model: db.Role, attributes: ["role_category"] }],
-				attributes: ["user_id"],
-			});
-			break;
-		case "TICKET_COMMENT":
-			const ticket = await db.Ticket.findOne({
-				where: { ticket_id: entity.ticket_id },
-			});
-			notifier = await db.User.findAll({
-				where: {
-					user_id:
-						ticket.pic_id === entity.user_id
-							? ticket.created_by
-							: ticket.pic_id,
-				},
-				attributes: ["user_id"],
-			});
-			break;
-		case "TICKET_PROGRESS":
-			notifier = await db.User.findAll({
-				where: {
-					user_id: entity.created_by,
-				},
-				attributes: ["user_id"],
-			});
-      break
-		case "TICKET_CLOSED":
-			notifier = await db.User.findAll({
-				where: {
-					user_id: entity.pic_id,
-				},
-				attributes: ["user_id"],
-			});
-			break;
-	}
-
-	const notification = await db.Notification.bulkCreate(
-		notifier.map((user) => ({
+	const trx = await db.sequelize.transaction();
+	try{
+		const notification_object = await db.NotificationObject.create({
+			entity_type: type,
+			entity_id: entity.id || entity.comment_id,
+		}, {
+			transaction: trx
+		});
+	
+		const notification_change = await db.NotificationChange.create({
 			notification_object_id: notification_object.id,
-			notifier_id: user.user_id,
-		}))
-	);
+			actor_id: type === "TICKET_PROGRESS" ? entity.pic_id : entity.created_by || entity.user_id,
+		}, {
+			transaction: trx
+		});
+	
+		let notifier;
+	
+		switch (type) {
+			case "TICKET_CREATE":
+				notifier = await db.User.findAll({
+					where: {
+						"$Role.role_category$": entity.assigned_to,
+					},
+					include: [{ model: db.Role, attributes: ["role_category"] }],
+					attributes: ["user_id"],
+				});
+				break;
+			case "TICKET_COMMENT":
+				const ticket = await db.Ticket.findOne({
+					where: { ticket_id: entity.ticket_id },
+				});
+				notifier = await db.User.findAll({
+					where: {
+						user_id:
+							ticket.pic_id === entity.user_id
+								? ticket.created_by
+								: ticket.pic_id,
+					},
+					attributes: ["user_id"],
+				});
+				break;
+			case "TICKET_PROGRESS":
+				notifier = await db.User.findAll({
+					where: {
+						user_id: entity.created_by,
+					},
+					attributes: ["user_id"],
+				});
+				break
+			case "TICKET_CLOSED":
+				notifier = await db.User.findAll({
+					where: {
+						user_id: entity.pic_id,
+					},
+					attributes: ["user_id"],
+				});
+				break;
+		}
+	
+		const notification = await db.Notification.bulkCreate(
+			notifier.map((user) => ({
+				notification_object_id: notification_object.id,
+				notifier_id: user.user_id,
+			})), {transaction: trx}
+		);
+		await trx.commit();
+	} catch (error) {
+		await trx.rollback();
+		throw error;
+	}
+	
 
-	return notification_object;
+	return;
 };
 
 const getUserNotification = async (req, res, next) => {
 	const notifications = await db.Notification.findAll({
+		limit: 10,
     order: [["createdAt", "DESC"]],
 		where: {
 			notifier_id: req.userData.id,
@@ -108,6 +121,7 @@ const getUserNotification = async (req, res, next) => {
 				});
         payload = {
           ticket_id: ticket.ticket_id,
+					event_location: ticket.location,
           product_name: ticket.Product.product_name,
           subproduct_name: ticket.Subproduct.subproduct_name,
           subject: ticket.CaseSubject.subject,
@@ -133,8 +147,14 @@ const getUserNotification = async (req, res, next) => {
 		};
 	});
 
+	res.status(200).json( await Promise.all(notification_objects) );
 
-	return res.status(200).json( await Promise.all(notification_objects) );
+	notifications.forEach(async n => {
+		n.status = "READ"
+		await n.save()
+	})
+
+	return
 };
 
 module.exports = {
